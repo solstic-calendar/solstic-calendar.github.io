25043a3c3de2b1ea5f123c729318eab2a5d948c7
Thomas Buckley-Houston <tom@tombh.co.uk>
2026-03-15 15:51:28 +0000

    Months runes

--- main.js
+++ main.js
@@ -1,17 +1,60 @@
+const ASSETS_PATH = "assets";
+
+const SVGS = {};
+
 const MONTHS = [
-	{ name: "Hazel" },
-	{ name: "Birch" },
-	{ name: "Alder" },
-	{ name: "Hawthorn" },
-	{ name: "Oak" },
-	{ name: "Ash" },
-	{ name: "Rowan" },
-	{ name: "Apple" },
-	{ name: "Beech" },
-	{ name: "Willow" },
-	{ name: "Yew" },
-	{ name: "Holly" },
-	{ name: "Pine" },
+	{
+		name: "Hazel",
+		rune: "x",
+	},
+	{
+		name: "Birch",
+		rune: "b",
+	},
+	{
+		name: "Alder",
+		rune: "E",
+	},
+	{
+		name: "Hawthorn",
+		rune: "T",
+	},
+	{
+		name: "Oak",
+		rune: "d",
+	},
+	{
+		name: "Ash",
+		rune: "a",
+	},
+	{
+		name: "Rowan",
+		rune: "r",
+	},
+	{
+		name: "Apple",
+		rune: "p",
+	},
+	{
+		name: "Beech",
+		rune: "J",
+	},
+	{
+		name: "Willow",
+		rune: "w",
+	},
+	{
+		name: "Yew",
+		rune: `${ASSETS_PATH}/yew.svg`,
+	},
+	{
+		name: "Holly",
+		rune: "H",
+	},
+	{
+		name: "Pine",
+		rune: "c",
+	},
 ];
 
 const DAYS = [
@@ -58,9 +101,11 @@ const CALENDAR_START = "Feburary 5";
 
 main();
 
-function main() {
+async function main() {
 	let day;
 	let month;
+
+	await loadAllSVGs();
 
 	for (let index = 0; index <= daysSince(); index++) {
 		day = getDay(index);
@@ -69,8 +114,24 @@ function main() {
 	}
 
 	document.getElementById("solstic_month").textContent = month.name;
+	document.getElementById("solstic_rune").textContent = month.rune;
 	document.getElementById("solstic_number").textContent = number;
 	document.getElementById("solstic_day").textContent = day.name;
+	document.getElementById("all_rune_months").innerHTML =
+		listMonthRunes().join(" ");
+}
+
+async function loadAllSVGs() {
+	const loaders = MONTHS.filter((month) =>
+		month?.rune?.includes(ASSETS_PATH),
+	).map((month) => loadSVG(month.rune));
+
+	await Promise.allSettled(loaders);
+}
+
+async function loadSVG(path) {
+	const response = await fetch(path);
+	SVGS[path] = await response.text();
 }
 
 function getDay(index) {
@@ -129,4 +190,14 @@ function isMossDay(index) {
 function isLeapYear() {
 	const thisYear = new Date().getFullYear();
 	return new Date(thisYear, 1, 29).getDate() === 29;
+}
+
+function listMonthRunes() {
+	return Object.values(MONTHS).map((month) => {
+		if (!month.rune.includes(ASSETS_PATH)) {
+			return month.rune;
+		} else {
+			return SVGS[month.rune];
+		}
+	});
 }
